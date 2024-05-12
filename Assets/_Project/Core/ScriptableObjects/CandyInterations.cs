9debed20cc0d55aabef12853b1e2f667117ecae3
Haidon Cosse <Haidonmcosse@gmail.com>
2024-05-12 12:48:18 -0500

    added comments

--- Assets/_Project/Core/ScriptableObjects/CandyInterations.cs
+++ Assets/_Project/Core/ScriptableObjects/CandyInterations.cs
@@ -7,41 +7,80 @@ using UnityEngine.InputSystem;
 public class CandyInterations : MonoBehaviour
 {
 
+    // Input action for mouse click
     private InputAction _mouseClick;
+
+    // Rigidbody of the game object
     private Rigidbody _rigidbody;
+
+    // Speed of mouse drag physics
     [SerializeField]
     private float _mouseDragPhysicsSpeed = 10;
+
+    // Speed of mouse drag
     [SerializeField]
     private float _mouseDragSpeed = .1f;
+
+    // Location to give bag
     [SerializeField]
     private GameObject _giveBaglocation;
+
+    // Script for door interaction
     [SerializeField]
     private DoorInteractionScript _doorInteraction;
+
+    // Flag to check if candy is monster candy
     [SerializeField]
     private bool _isMonsterCandy;
+
+    // Starting position of the game object
     private Vector3 _startingPosition;
+
+    // Main camera of the game
     private Camera _mainCamera;
+
+    // Velocity of the game object
     private Vector3 _velocity;
+
+    // Timer for points
     private PointsTimer _pointsTimer;
+
+    // Count of monster candy
     private float _monsterCandyCount = 0;
+
+    // Count of human candy
     private float _humanCandyCount = 0;
+
+    // Script for child interactions
     private ChildInteractions _childInteractions;
+
+    // Script for interaction
     private Interactionscript _interactionscript;
-    public float MonsterCandy 
+
+    // Property for monster candy count
+    public float MonsterCandy
     {
         get { return _monsterCandyCount; }
         set { _monsterCandyCount = value; }
     }
+
+    // Property for human candy count
     public float HumanCandy
     {
         get { return _humanCandyCount; }
         set { _humanCandyCount = value; }
     }
+
+    // Property to check if candy is monster candy
     public bool IsMonster
     {
         get { return _isMonsterCandy; }
     }
+
+    // Wait for fixed update
     private WaitForFixedUpdate _waitForFixedUpdate = new WaitForFixedUpdate();
+
+    // Awake function to initialize variables
     private void Awake()
     {
         _interactionscript = FindObjectOfType<Interactionscript>();
@@ -54,24 +93,29 @@ public class CandyInterations : MonoBehaviour
         _childInteractions = FindObjectOfType<ChildInteractions>();
     }
 
+    // Enable mouse click input action
     private void OnEnable()
     {
         _mouseClick.Enable();
         _mouseClick.performed += MousePressed;
-        
+
     }
+
+    // Disable mouse click input action
     private void OnDisable()
     {
         _mouseClick.performed -= MousePressed;
         _rigidbody.useGravity = true;
         _mouseClick.Disable();
     }
+
+    // Function to handle mouse press event
     public void MousePressed(InputAction.CallbackContext context)
     {
         Ray ray = _mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
 
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit)) 
+        if (Physics.Raycast(ray, out hit))
         {
             if (hit.collider != null && (hit.collider.gameObject.CompareTag("Candy")))
             {
@@ -83,6 +127,8 @@ public class CandyInterations : MonoBehaviour
 
 
     }
+
+    // Update function to handle gravity
     private void Update()
     {
         if (_mouseClick.ReadValue<float>() != 0)
@@ -91,6 +137,8 @@ public class CandyInterations : MonoBehaviour
 
         }
     }
+
+    // Function to handle collision enter event
     private void OnCollisionEnter(Collision other)
     {
 
@@ -152,11 +200,13 @@ public class CandyInterations : MonoBehaviour
             }
         }
     }
+
+    // Coroutine to handle drag update
     private IEnumerator DragUpdate(GameObject clickedObject)
     {
         float _initialDistance = Vector3.Distance(clickedObject.transform.position, _mainCamera.transform.position);
         clickedObject.TryGetComponent<Rigidbody>(out Rigidbody rb);
-        while (_mouseClick.ReadValue<float>() != 0) 
+        while (_mouseClick.ReadValue<float>() != 0)
         {
             Ray ray = _mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
             if (rb != null)
