cfd0956d58545ef7b85e7d5c0f49fd75332af972
Paris Rainey <paris.c.rainey@gmail.com>
2024-05-13 14:28:22 -0500

    Fixes: Began bug fixing spawning
    Finalized Child Controller,
    Began bug fixing spawn system to spawn child

--- Assets/_Project/Core/Managers/ChildManager.cs
+++ Assets/_Project/Core/Managers/ChildManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
+[RequireComponent (typeof (SphereCollider))]
 public class ChildManager : MonoBehaviour
 {
     [SerializeField]
-    private bool _isChildSpawned;
+    private bool _isChildSpawned = false;
     [Tooltip("The object that will be instantiated.")]
     [SerializeField]
     private GameObject _spawnObject;
@@ -13,22 +15,38 @@ public class ChildManager : MonoBehaviour
     private GameObject _spawner;
     [Tooltip("The amount of time in seconds between each spawn.")]
     [SerializeField]
-    private float _timeBetweenSpawns;
+    private float _timeBetweenSpawns = 6f;
     [Tooltip("If false, the spawner will stop instantiating clones of the reference.")]
     [SerializeField]
-    private bool _canSpawn;
+    private bool _canSpawn = true;
 
     private int _ChildCount = 10;
+    private SphereCollider _sphereCollider;
+    
     // Start is called before the first frame update
     void Start()
     {
-        if (_isChildSpawned && _canSpawn)
+        if (_ChildCount !< 10 && _isChildSpawned && _canSpawn)
             StartCoroutine(SpawnObjects());
     }
 
     void SetCanSpawn()
     {
         _canSpawn = true;
+    }
+
+    bool CheckSpawnTrigger()
+    {
+        if (_sphereCollider.isTrigger == true)
+        {
+            _isChildSpawned = true;
+            _canSpawn = false;
+            return true;
+        }
+
+        _canSpawn = true;
+        _isChildSpawned = false;
+        return false;
     }
 
     /// <summary>
@@ -43,6 +61,8 @@ public class ChildManager : MonoBehaviour
             GameObject spawnedEnemy = Instantiate(_spawnObject, transform.position, new Quaternion());
             //Pause for the given time in seconds before resuming the function
             yield return new WaitForSeconds(_timeBetweenSpawns);
+            //Subtract from child count
+            _ChildCount--;
         }
     }
 }
