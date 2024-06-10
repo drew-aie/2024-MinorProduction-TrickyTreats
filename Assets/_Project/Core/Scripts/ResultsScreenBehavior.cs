7c415be4b74cc5d501606bc58ebcd360e12f704e
Reginald Thompson <s238002@students.aie.edu>
2024-06-10 15:41:00 -0500

    Added Results screen functionality.

---
+++ Assets/_Project/Core/Scripts/ResultsScreenBehavior.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class ResultsScreenBehavior : MonoBehaviour
+{
+    [SerializeField]
+    TMP_Text Results;
+
+    // Start is called before the first frame update
+    //void Start()
+    //{
+    //    _ScoreManager = FindObjectOfType<ScoreManager>();
+    //}
+
+    //// Update is called once per frame
+    void Update()
+    {
+        Results.text = "Total Score: " + " <br>";
+    }
+
+
+    public void BackToMain()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+
+}
